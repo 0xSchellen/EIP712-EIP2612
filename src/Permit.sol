5b5df19d46d3fd9b44032975348e24d0169195a0
Carlos Schellenberger <carlos.schellenberger#gmail.com>
2022-07-22 14:00:38 -0300

    Initial commit

--- src/Permit.sol
+++ src/Permit.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract EIP712 {
+contract Permit {
 
     /*//////////////////////////////////////////////////////////////
                              EIP-2612 LOGIC
